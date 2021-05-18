718fab8ea3f954594c0075492f9e413201c0ab45
kazejin19 <ckevin.enoz@gmail.com>
2021-05-19 05:35:54 +0800

    -Creating spelling checker page

---
+++ src/system/pages/spellchecker.js
@@ -0,0 +1,25 @@
+import React,{Component} from "react";
+
+
+const TableView=(props)=>{
+    
+    return (
+        <table className="form "></table>
+    );
+
+}
+
+class SpellChecker extends Component {
+
+    render(){
+        
+        return(<>
+            <div className="container-md d-flex flex-wrap flex-row justify-content-center justify-content-md-start">
+                
+            </div>
+        </>);
+    }
+
+}
+
+export default SpellChecker;
